711f8d2b6b2bb77d51b601c4771fc218d124f643
Abdul Wasay <abbasiwasay302@gmail.com>
2026-09-18 18:34:42 +0500

    update

--- office-portal/db.php
+++ office-portal/db.php
@@ -1,9 +1,5 @@
 <?php
-$host="localhost";
-$user="root";
-$password="";
-$db="login_system";
-
+require 'config.php';
 $connection=mysqli_connect($host,$user,$password,$db,4306);
 if(!$connection){
     die("connection failed" . mysqli_connect_error());
